9bf7e67d7bdade99dec96e9c23202ccab3b6c6a3
Luan Victor <luanvictorfsilva@gmail.com>
2026-08-24 22:03:37 -0300

    Tabuada em JS

--- Ex013/Tabuada/script.js
+++ Ex013/Tabuada/script.js
@@ -1,1 +1,18 @@
-alert ("Hello, World!");
+function tabuada(){
+ let numero = document.getElementById("txtn")
+ let tabuada = document.getElementById('tab')
+ if (numero.value.length == 0){
+    window.alert('Informe um número!')}
+ else{   
+ let n = Number(numero.value)
+ tabuada.innerHTML=''   
+ for(let contador = 1; contador<=10; contador+=1){
+    let item = document.createElement('option')  
+    item.text = `${n} x ${contador} = ${n*contador}` 
+    item.value = `tab${contador}`
+    tab.appendChild(item) 
+    
+}
+}
+
+}
